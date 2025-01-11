72cb2b08f1127078091a4e859b55883150b29d3e
kennycallado <kennycallado@hotmail.com>
2025-01-11 16:52:59 +0100

    UPDATE
    reuse conts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig, sharpImageService } from "astro/config";
+import { WEBSLAB_PROJECT } from "./src/_includes/lib/consts.ts";
 
 import lit from "@astrojs/lit";
 import purgecss from "astro-purgecss";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-	site: "https://impulsa.lab.kennycallado.dev",
+	site: `https://${WEBSLAB_PROJECT}.ipsitec.es`,
 	server: {
 		port: 3000,
 	},
--- src/_includes/lib/consts.ts
+++ src/_includes/lib/consts.ts
@@ -1,3 +1,7 @@
+export const WEBSLAB_PROJECT = "impulsa";
+export const WEBSLAB_DOMAIN = "ipsitec.es";
+export const WEBSLAB_TOKEN = "TOKEN"; // TODO: temporary, should be user token
+
 export const SITE_TITLE = "Impulsa";
 export const SITE_DESCRIPTION = "Welcome to Impulsa!";
 
@@ -6,16 +10,7 @@ export const DB = {
 	url: "ws://localhost:8000/",
 	config: {
 		access: "user",
-		database: "impulsa",
+		database: WEBSLAB_PROJECT,
 		namespace: "webslab",
 	},
 };
-
-// export const DB: DbConfig = {
-// 	url: 'https://db.lab.kennycallado.dev/',
-// 	config: {
-// 		access: 'user',
-// 		database: 'impulsa',
-// 		namespace: 'webslab',
-// 	},
-// }
